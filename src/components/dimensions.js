e294d147d66876b87544627f942fde0360563e4c
LucasBKing <ldsbarbosa@inf.ufpel.edu.br>
2019-06-21 11:42:30 -0300

    adding a function that resize the traffic graph component

--- src/components/dimensions.js
+++ src/components/dimensions.js
@@ -4,10 +4,36 @@ export default function useWindowSize() {
   const isClient = typeof window === 'object';
 
   function getSize() {
-    return {
-      width: isClient ? window.innerWidth : undefined,
-      height: isClient ? window.innerHeight : undefined
+    const widthSize = isClient ? window.innerWidth : undefined;
+    const size = {
+      width: '',
+      height: ''
     };
+
+    if (widthSize >= 991) {
+      size.width = 644;
+      size.height = 373;
+    }
+    
+    if (widthSize <= 990) {
+      size.width = 410;
+      size.height = 380;
+    }
+    
+    if (widthSize <= 880) {
+      size.width = 330;
+      size.height = 250;
+    }        
+    if (widthSize <= 719) {
+      size.width = 260;
+      size.height = 250;
+    }
+    if (widthSize <= 559) {
+      size.width = 300;
+      size.height = 250;
+    }
+
+    return size;
   }
 
   const [windowSize, setWindowSize] = useState(getSize);
--- src/components/traffic.component.js
+++ src/components/traffic.component.js
@@ -4,59 +4,26 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { getSales } from '../functions/traffic';
 import '../assets/css/traffic.css'
 
-
 export default function TrafficGraph() {
-    const dimensions = useWindowSize();
+    const windowSize = useWindowSize();
+
     const [trafficData, setTrafficData] = useState([]);
-    const [widthGraph, setWidthGraph] = useState();
-    const [heightGraph, setheightGraph] = useState();
-    
+
     useEffect(() => {
         async function fetchData() {
             const response = await getSales();
             setTrafficData(response);
         }
 
-        fetchData();
-        getDimensions();
- 
-        return () => {
-            getDimensions();
-        }
+        fetchData();        
     }, []);
-  
-    function getDimensions() {
-        if (dimensions.width >= 991) {
-            setWidthGraph(644);
-            setheightGraph(373);
-        }
-        
-        if (dimensions.width <= 990) {
-            setWidthGraph(410);
-            setheightGraph(380);
-        }
-        
-        if (dimensions.width <= 880) {
-            setWidthGraph(330);
-            setheightGraph(250);
-        }        
-        if (dimensions.width <= 719) {
-            setWidthGraph(260);
-        }
-        if (dimensions.width <= 559) {
-            setWidthGraph(300);
-        }
-
-        console.log(dimensions.width);
-        
-    }
   
     return (
         <div className="trafficDiv">
             <h3>Site Traffic Overview</h3>
             <BarChart
-                width={widthGraph}
-                height={heightGraph}
+                width={windowSize.width}
+                height={windowSize.height}
                 data={trafficData}
                 margin={{
                     top: 5, right: 30, left: 5, bottom: 5,
@@ -69,7 +36,6 @@ export default function TrafficGraph() {
                 <Tooltip />
                 <Bar dataKey="sales" fill="#4c84ff" />   
             </BarChart>
-
         </div>
     );
 }
